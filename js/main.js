e1bb7dce17609e02209930706fa63c644f7a74b4
GiovanaGama <giovana.gama.dc@gmail.com>
2023-08-08 13:08:51 -0300

    New feature

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const controle = document.querySelectorAll('[data-controle]')
 const estatisticas = document.querySelectorAll('[data-estatistica]')
-console.log(estatisticas)
+let imagem = document.querySelector('#image')
+const botoes = document.querySelectorAll('.alterar')
 
 const pecas = {
     "bracos": {
@@ -36,6 +37,55 @@ const pecas = {
     }
 }
 
+botoes.forEach( (botao) =>{
+    botao.addEventListener('click', (evento) =>{
+        trocarCor(evento.target.id)
+    })
+})
+
+function trocarCor(corNova) {
+    let arquivo = (`../cores/${corNova}.png`)
+    imagem.setAttribute('src', arquivo)
+}
+
+
+controle.forEach( (elemento) =>{ //arrow function é um dos jeitos de declarar funções anonimas
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode, evento.target.dataset.peca)
+    })
+})
+
+function manipulaDados(operacao, controle, pecaAlterada) {
+
+    const pecaEspecifica = controle.querySelector('[data-contador]')
+    if (operacao === "-") {
+        if (pecaEspecifica.value == 0) {
+            alert('Não pode')
+        } else {
+        pecaEspecifica.value = parseInt(pecaEspecifica.value) - 1
+        estatisticas.forEach( (stat) => {
+            stat.textContent = parseInt(stat.textContent) - pecas[pecaAlterada][stat.dataset.estatistica]
+        })
+        }
+    } else {
+        pecaEspecifica.value = parseInt(pecaEspecifica.value) + 1
+        estatisticas.forEach( (stat) => {
+            stat.textContent = parseInt(stat.textContent) + pecas[pecaAlterada][stat.dataset.estatistica]
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 /* 
 controle.forEach( (elemento) =>{ //arrow function é um dos jeitos de declarar funções anonimas
     elemento.addEventListener('click', (evento) => {
@@ -66,29 +116,3 @@ function alterarEstatistica (pecaAlterada) {
 
 
  */
-
-controle.forEach( (elemento) =>{ //arrow function é um dos jeitos de declarar funções anonimas
-    elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode, evento.target.dataset.peca)
-    })
-})
-
-function manipulaDados(operacao, controle, pecaAlterada) {
-
-    const pecaEspecifica = controle.querySelector('[data-contador]')
-    if (operacao === "-") {
-        if (pecaEspecifica.value == 0) {
-            alert('Não pode')
-        } else {
-        pecaEspecifica.value = parseInt(pecaEspecifica.value) - 1
-        estatisticas.forEach( (stat) => {
-            stat.textContent = parseInt(stat.textContent) - pecas[pecaAlterada][stat.dataset.estatistica]
-        })
-        }
-    } else {
-        pecaEspecifica.value = parseInt(pecaEspecifica.value) + 1
-        estatisticas.forEach( (stat) => {
-            stat.textContent = parseInt(stat.textContent) + pecas[pecaAlterada][stat.dataset.estatistica]
-        })
-    }
-}
